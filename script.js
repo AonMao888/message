5b80696331d8a721a1335136c79cc992b3889ecb
Aon Mao <96533871+AonMao888@users.noreply.github.com>
2022-07-03 07:29:59 -0700

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-
-const inp = document.querySelector("input");
-const btn = document.querySelector("button");
-
-btn.addEventListener("click",()=>{
-    window.location.href = `http://localhost:4000/download?URL=${inp.value}`;
-})
